c11ed63ee912a6d84ce37b31ca46e980e450a45a
PC\st <dexterword@gmail.com>
2021-10-11 12:58:12 +0900

    Update FDTestActivity.java

--- FDTestActivity.java
+++ FDTestActivity.java
@@ -3,7 +3,7 @@ package com.example.f_dload;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
+import android.content.pm.ActivityInfo;	//
 import android.os.Bundle;
 import android.view.KeyEvent;
 
